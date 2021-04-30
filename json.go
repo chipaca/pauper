2a58922ee3a36391f0044005ba3383022099e0b5
John R. Lenton <jlenton@gmail.com>
2021-04-30 21:32:46 +0100

    added GetInt, fixed GetFloat, but commented it out because buggy and unneeded

--- json.go
+++ json.go
@@ -1,7 +1,6 @@
 package pauper
 
 import (
-	"strconv"
 	"unicode/utf16"
 	"unicode/utf8"
 )
@@ -126,48 +125,163 @@ func u4(buf []byte, start int) rune {
 	return r
 }
 
-func GetNumber(buf []byte) (float64, int, error) {
+// GetInt parses a subset of the JSON 'number', namely the 'integer'
+// part of it. If there's a decimal point or an exponent it will
+// fail. If there are too many digits for an int64 it'll fail.
+func GetInt(buf []byte) (int64, int, error) {
 	start := SkipWS(buf)
 	i := start
 	if len(buf) <= i {
 		return 0, 0, ErrNoNumberHere
 	}
-	// sign
+	var neg bool
 	if buf[i] == '-' {
 		i++
+		if len(buf) <= i {
+			return 0, 0, ErrNoNumberHere
+		}
+		neg = true
+	}
+	var n int64
+	for m := i + 20; i < m && len(buf) > i && '0' <= buf[i] && buf[i] <= '9'; i++ {
+		n *= 10
+		n += int64(buf[i] - '0')
+	}
+	if neg {
+		n = -n
+	}
+	if len(buf) <= i {
+		return n, i, nil
+	}
+	// if the number is finished, the next byte will be
+	// whitespace, or a comma, or a closing bracket or brace
+	switch buf[i] {
+	case ' ', '\n', '\r', '\t', ',', ']', '}':
+		return n, i, nil
+	}
+	return n, i, ErrNoNumberHere
+}
+
+/*
+
+var float64pow10 = []float64{
+	1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9,
+	1e10, 1e11, 1e12, 1e13, 1e14, 1e15, 1e16, 1e17, 1e18, 1e19,
+	1e20, 1e21, 1e22,
+}
+
+func GetFloat(buf []byte) (float64, int, error) {
+	start := SkipWS(buf)
+	i := start
+	if len(buf) <= i {
+		return 0, 0, ErrNoNumberHere
+	}
+
+	// simplified from strconv.ParseFloat
+
+	// sign
+	var neg bool
+	if buf[i] == '-' {
+		i++
+		neg = true
 	}
 	if len(buf) <= i {
 		return 0, 0, ErrNoNumberHere
 	}
-	// integer
-	switch buf[i] {
-	case '0':
-		i++
-	case '1', '2', '3', '4', '5', '6', '7', '8', '9':
-		i++
-		for len(buf) > i && buf[i] >= '0' && buf[i] <= '9' {
-			i++
+	// mantissa
+	const maxMantDigits = 19
+	nd := 0     // the number of digits seen
+	ndMant := 0 // the number in the mantissa
+	dp := 0     // the position of the '.'
+	var mantissa uint64
+	sawdot := false
+	sawdig := false
+	for ; len(buf) > i; i++ {
+		switch c := buf[i]; true {
+		case c == '.':
+			if sawdot {
+				return 0, 0, ErrNoNumberHere
+			}
+			sawdot = true
+			dp = nd
+			continue
+		case '0' <= c && c <= '9':
+			sawdig = true
+			if c == '0' && nd == 0 {
+				dp--
+				continue
+			}
+			nd++
+			if ndMant < maxMantDigits {
+				mantissa *= 10
+				mantissa += uint64(c - '0')
+				ndMant++
+			}
+			continue
 		}
-	default:
+		break
+	}
+	if !sawdig {
 		return 0, 0, ErrNoNumberHere
 	}
-	if len(buf) > i && buf[i] == '.' {
-		// fraction
-		i++
-		for len(buf) > i && buf[i] >= '0' && buf[i] <= '9' {
-			i++
-		}
+	if !sawdot {
+		dp = nd
 	}
-	if len(buf) > i && buf[i]|32 == 'e' {
-		// exponent
+	if len(buf) > i && (buf[i] == 'e' || buf[i] == 'E') {
 		i++
-		if len(buf) > i && (buf[i] == '+' || buf[i] == '-') {
+		if len(buf) <= i {
+			return 0, 0, ErrNoNumberHere
+		}
+		esign := 1
+		switch buf[i] {
+		case '-':
+			esign = -1
+			fallthrough
+		case '+':
 			i++
 		}
-		for len(buf) > i && buf[i] >= '0' && buf[i] <= '9' {
-			i++
+		if i >= len(buf) || buf[i] < '0' || buf[i] > '9' {
+			return 0, 0, ErrNoNumberHere
 		}
+		e := 0
+		for ; i < len(buf) && '0' <= buf[i] && buf[i] <= '9'; i++ {
+			if e < 10000 {
+				e = e*10 + int(buf[i]-'0')
+			}
+		}
+		dp += e * esign
 	}
-	f, err := strconv.ParseFloat(string(buf[start:i]), 64)
-	return f, i, err
+
+	var exp int
+	if mantissa != 0 {
+		exp = dp - ndMant
+	}
+
+	f := float64(mantissa)
+	if neg {
+		f = -f
+	}
+	var float64pow10 = []float64{
+		1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9,
+		1e10, 1e11, 1e12, 1e13, 1e14, 1e15, 1e16, 1e17, 1e18, 1e19,
+		1e20, 1e21, 1e22,
+	}
+
+	switch {
+	case exp > 0:
+		for exp > 22 {
+			exp -= 22
+			f *= float64pow10[22]
+		}
+		f *= float64pow10[exp]
+	case exp < 0:
+		for exp < -22 {
+			exp += 22
+			f /= float64pow10[22]
+		}
+		f /= float64pow10[-exp]
+	}
+	return f, i, nil
 }
+
+*/
--- json_test.go
+++ json_test.go
@@ -36,12 +36,12 @@ func BenchmarkSkipWS(b *testing.B) {
 
 func TestGetString(t *testing.T) {
 	for from, expected := range map[string]string{
-		`"hello"`:  "hello",
-		`   "hi"`:  "hi",
-		` "hi\""`:  "hi\"",
-		`     ""`:  "",
-		`"árbol"`:  "árbol",
-		`"\u0020"`: " ",
+		`"hello"`:           "hello",
+		`   "hi"`:           "hi",
+		` "hi\""`:           "hi\"",
+		`     ""`:           "",
+		`"árbol"`:           "árbol",
+		`"\u0020"`:          " ",
 		`"\uD83D\uDCA9 hi"`: "💩 hi",
 	} {
 		t.Run(from, func(t *testing.T) {
@@ -74,50 +74,212 @@ func TestGetString(t *testing.T) {
 }
 
 func BenchmarkGetString_noEscapes(b *testing.B) {
-	buf := []byte(`"1234567890qwertyuiopasdfghjklzxcvbnm áéíóú"`)
 	for i := 0; i < b.N; i++ {
-		pauper.GetString(buf)
+		pauper.GetString([]byte(`"1234567890qwertyuiopasdfghjklzxcvbnm áéíóú"`))
 	}
 }
 func BenchmarkGetString(b *testing.B) {
-	buf := []byte(`"1234567890qwertyuiopasdfghjklzxcvbnm áéíóú \uD83D\uDCA9 \"💩\""`)
-	//buf := []byte(`"1234567890qwertyuiopasdfghjklzxcvbnm áéíóú"`)
 	for i := 0; i < b.N; i++ {
-		pauper.GetString(buf)
+		pauper.GetString([]byte(`"1234567890qwertyuiopasdfghjklzxcvbnm áéíóú \uD83D\uDCA9 \"💩\""`))
 	}
 }
 
-func TestGetNumber(t *testing.T) {
-	for from, expected := range map[string]float64{
-		"0,": 0,
-		" -0,": 0,
-		"10,": 10,
-		"12345678900000000000000000000000000000000000000000000000000000000000000000000000000000000000,": 1.23456789e91,
-		"1.23,": 1.23,
-		"1.23e4,": 12300,
-		"1.23e+4,": 12300,
-		"123e4,": 1230000,
-		"123e-2,": 1.23,
+func TestGetInt(t *testing.T) {
+	for from, expected := range map[string]int64{
+		"0":                    0,
+		"1":                    1,
+		"1000":                 1000,
+		"9223372036854775807":  1<<63 - 1,
+		"-0":                   0,
+		"-1":                   -1,
+		"-1000":                -1000,
+		"-9223372036854775807": -(1<<63 - 1),
+		"-9223372036854775808": -1 << 63,
 	} {
-		t.Run(from, func(t *testing.T) {
-			buf := []byte(from)
-			got, i, err := pauper.GetNumber(buf)
-			if err != nil {
-				t.Error(err)
+		for _, last := range []string{"", ",", "]", "}", " "} {
+			for _, start := range []string{"", " ", " \n\t\r"} {
+				in := start + from + last
+				t.Run(in, func(t *testing.T) {
+					f, i, e := pauper.GetInt([]byte(in))
+					if e != nil {
+						t.Fatal(e)
+					}
+					if f != expected {
+						t.Errorf("expected %d, got %d", expected, f)
+					}
+					if last != "" {
+						if in[i] != last[0] {
+							t.Errorf("expected i to point to the ending %s in %q, got %d (%c)", last, in, i, from[i])
+						}
+					}
+				})
 			}
-			if buf[i] != ',' {
-				t.Errorf("%q at %d?", buf, i)
-			}
-			if got != expected {
-				t.Errorf("%g != %g", got, expected)
-			}
-		})
+		}
 	}
 }
 
-func BenchmarkGetNumber(b *testing.B) {
-	buf := []byte(`-12345678900000000000000000000000000000000000000000000000000000000000000000000000000000000000.123E-10, "hi"`)
+func BenchmarkGetInt(b *testing.B) {
+	buf := []byte("   -9223372036854775808   ")
 	for i := 0; i < b.N; i++ {
-		pauper.GetNumber(buf)
+		pauper.GetInt(buf)
 	}
 }
+
+// func TestGetFloat(t *testing.T) {
+// 	for from, expected := range map[string]float64{
+// 		"0,":   0,
+// 		" -0,": 0,
+// 		"10,":  10,
+// 		"12345678900000000000000000000000000000000000000000000000000000000000000000000000000000000000,": 1.23456789e91,
+// 		"1.23,":    1.23,
+// 		"1.23e4,":  12300,
+// 		"1.23e+4,": 12300,
+// 		"123e4,":   1230000,
+// 		"123e-2,":  1.23,
+// 	} {
+// 		t.Run(from, func(t *testing.T) {
+// 			buf := []byte(from)
+// 			got, i, err := pauper.GetFloat(buf)
+// 			if err != nil {
+// 				t.Fatal(err)
+// 			}
+// 			if buf[i] != ',' {
+// 				t.Errorf("%q at %d?", buf, i)
+// 			}
+// 			if got != expected {
+// 				t.Errorf("%g != %g", got, expected)
+// 			}
+// 		})
+// 	}
+// }
+
+// type atofTest struct {
+// 	in  string
+// 	out float64
+// }
+
+// var ErrSyntax = pauper.Error("x")
+// var ErrRange = pauper.Error("y")
+
+// // from strconv's atof tests, pruned
+// var atofTests = []atofTest{
+// 	{"1", 1},
+// 	{"1e23", 1e+23},
+// 	{"1E23", 1e+23},
+// 	{"100000000000000000000000", 1e+23},
+// 	{"1e-100", 1e-100},
+// 	{"123456700", 1.234567e+08},
+// 	{"99999999999999974834176", 9.999999999999997e+22},
+// 	{"100000000000000000000001", 1.0000000000000001e+23},
+// 	{"100000000000000008388608", 1.0000000000000001e+23},
+// 	{"100000000000000016777215", 1.0000000000000001e+23},
+// 	{"100000000000000016777216", 1.0000000000000003e+23},
+// 	// {"-1", "-1"},
+// 	// {"-0.1", "-0.1"},
+// 	// {"-0", "-0"},
+// 	// {"1e-20", "1e-20"},
+// 	// {"625e-3", "0.625"},
+
+// 	// // zeros
+// 	// {"0", "0"},
+// 	// {"0e0", "0"},
+// 	// {"-0e0", "-0"},
+// 	// {"+0e0", "0"},
+// 	// {"0e-0", "0"},
+// 	// {"-0e-0", "-0"},
+// 	// {"+0e-0", "0"},
+// 	// {"0e+0", "0"},
+// 	// {"-0e+0", "-0"},
+// 	// {"+0e+0", "0"},
+// 	// {"0e+01234567890123456789", "0"},
+// 	// {"0.00e-01234567890123456789", "0"},
+// 	// {"-0e+01234567890123456789", "-0"},
+// 	// {"-0.00e-01234567890123456789", "-0"},
+
+// 	// {"0e291", "0"}, // issue 15364
+// 	// {"0e292", "0"}, // issue 15364
+// 	// {"0e347", "0"}, // issue 15364
+// 	// {"0e348", "0"}, // issue 15364
+// 	// {"-0e291", "-0"},
+// 	// {"-0e292", "-0"},
+// 	// {"-0e347", "-0"},
+// 	// {"-0e348", "-0"},
+
+// 	// // largest float64
+// 	// {"1.7976931348623157e308", "1.7976931348623157e+308"},
+// 	// {"-1.7976931348623157e308", "-1.7976931348623157e+308"},
+
+// 	// // the border is ...158079
+// 	// // borderline - okay
+// 	// {"1.7976931348623158e308", "1.7976931348623157e+308"},
+// 	// {"-1.7976931348623158e308", "-1.7976931348623157e+308"},
+
+// 	// // a little too large
+// 	// {"1e308", "1e+308"},
+
+// 	// // denormalized
+// 	// {"1e-305", "1e-305"},
+// 	// {"1e-306", "1e-306"},
+// 	// {"1e-307", "1e-307"},
+// 	// {"1e-308", "1e-308"},
+// 	// {"1e-309", "1e-309"},
+// 	// {"1e-310", "1e-310"},
+// 	// {"1e-322", "1e-322"},
+// 	// // smallest denormal
+// 	// {"5e-324", "5e-324"},
+// 	// {"4e-324", "5e-324"},
+// 	// {"3e-324", "5e-324"},
+// 	// // too small
+// 	// {"2e-324", "0"},
+// 	// // way too small
+// 	// {"1e-350", "0"},
+// 	// {"1e-400000", "0"},
+
+// 	// // try to overflow exponent
+// 	// {"1e-4294967296", "0"},
+// 	// {"1e-18446744073709551616", "0"},
+
+// 	// // https://www.exploringbinary.com/java-hangs-when-converting-2-2250738585072012e-308/
+// 	// {"2.2250738585072012e-308", "2.2250738585072014e-308"},
+// 	// // https://www.exploringbinary.com/php-hangs-on-numeric-value-2-2250738585072011e-308/
+// 	// {"2.2250738585072011e-308", "2.225073858507201e-308"},
+
+// 	// // A very large number (initially wrongly parsed by the fast algorithm).
+// 	// {"4.630813248087435e+307", "4.630813248087435e+307"},
+
+// 	// // A different kind of very large number.
+// 	// {"22.222222222222222", "22.22222222222222"},
+// 	// {"2." + strings.Repeat("2", 4000) + "e+1", "22.22222222222222"},
+
+// 	// // Exactly halfway between 1 and math.Nextafter(1, 2).
+// 	// // Round to even (down).
+// 	// {"1.00000000000000011102230246251565404236316680908203125", "1"},
+// 	// // Slightly lower; still round down.
+// 	// {"1.00000000000000011102230246251565404236316680908203124", "1"},
+// 	// // Slightly higher; round up.
+// 	// {"1.00000000000000011102230246251565404236316680908203126", "1.0000000000000002"},
+// 	// // Slightly higher, but you have to read all the way to the end.
+// 	// {"1.00000000000000011102230246251565404236316680908203125" + strings.Repeat("0", 10000) + "1", "1.0000000000000002"},
+
+// 	// // Halfway between x := math.Nextafter(1, 2) and math.Nextafter(x, 2)
+// 	// // Round to even (up).
+// 	// {"1.00000000000000033306690738754696212708950042724609375", "1.0000000000000004"},
+// }
+
+// func TestGetFloatMore(t *testing.T) {
+// 	for _, tc := range atofTests {
+// 		t.Run(tc.in, func(t *testing.T) {
+// 			f, _, _ := pauper.GetFloat([]byte(tc.in))
+// 			if f != tc.out {
+// 				t.Errorf("got %g instead of %g", f, tc.out)
+// 			}
+// 		})
+// 	}
+// }
+
+// func BenchmarkGetFloat(b *testing.B) {
+// 	buf := []byte(`-12345678900000000000000000000000000000000000000000000000000000000000000000000000000000000000.123E-10, "hi"`)
+// 	for i := 0; i < b.N; i++ {
+// 		pauper.GetFloat(buf)
+// 	}
+// }
